14d729afdabac790054e8eedd16d544334fb5dca
Dan Sehl <dan.sehl@gmail.com>
2020-11-25 12:40:27 -0700

    Implement passing functionality for rows

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -6,3 +6,20 @@ To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
 
+class Matrix
+  attr_reader :rows
+  def initialize(input)
+    @orig_array = construct_array(input)
+    @rows = @orig_array
+  end
+
+  def construct_array(input)
+    array = []
+    input.lines.each do |element|
+      array << element.split
+    end
+    array.map! do |element|
+      element.map(&:to_i)
+    end
+  end
+end
