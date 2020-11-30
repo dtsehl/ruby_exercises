f4a7115557ddfeab06cb0429d9d285efd4a1d5ba
Dan Sehl <dan.sehl@gmail.com>
2020-11-30 11:06:58 -0700

    Unskip first 5 tests and write passing code

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -6,3 +6,12 @@ To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
 
+class Complement
+  def self.of_dna(dna)
+    return '' if dna.empty?
+    return 'G' if dna == 'C'
+    return 'C' if dna == 'G'
+    return 'A' if dna == 'T'
+    return 'U' if dna == 'A'
+  end
+end
